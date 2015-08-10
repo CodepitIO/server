6a2bf9d2c42fc523f4d155bfc8d543e95a2a93c4
Tomer Simis <tdl.simis@gmail.com>
2015-08-10 06:52:06 -0300

    Improving responsiveness os medium sized screens

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,11 +1,11 @@
-mrtApp.directive('loadingSpinner', function(){
-  return {
-    restrict: 'E',
-    scope: {
-      color: '=',
-      width: '=',
-      height: '='
-    },
-    templateUrl: 'views/misc/loading-spinner.html'
-  };
+mrtApp.directive('loadingSpinner', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			color: '=',
+			width: '=',
+			height: '='
+		},
+		templateUrl: 'views/misc/loading-spinner.html'
+	};
 });
