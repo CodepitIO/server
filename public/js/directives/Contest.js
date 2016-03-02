0f7d3b65fd1f82312b6f181e8bf3f128d72b0c99
vddm <vddm@cin.ufpe.br>
2016-03-02 02:54:57 -0300

    New directive mrtContestLabel wraps template for contest label

--- public/js/directives/Contest.js
+++ public/js/directives/Contest.js
@@ -1,8 +1,8 @@
 var app = angular.module('ContestDirective', []);
 
 /*
- * mrtContestListWrapper is the external template that wrappes other templates
- * it is responsable to maintain the proper struct and style of the templates it wrappes
+ * mrtContestListWrapper is the external template that wraps other templates
+ * it is responsable to maintain the proper structure and style of the templates it wraps
  * Set to be use as element-only
  * The transclude flag allows the directive to include templates inside it and creates a inherited child scope
  * The controller is added here because we need the filter functions on it
@@ -18,9 +18,29 @@ app.directive('mrtContestListWrapper', function() {
 });
 
 /*
+* mrtContestLabel is a directive only to wrap spans responsable to show the contest name and other structures
+*/
+app.directive('mrtContestLabel', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			linkable: "=", // Indicate if want the link to see the contest or just a span
+											// optinal - values = true/false
+			adminFlag: '=', // Span to indicate the user has admin properties over contest
+												// optinal - values = true/false
+			newFlag: '=', // Span to indicate that contest is new (based on function declared on controller)
+											// optinal - values = true/false
+			contest: "=", // A contest instance wich holds the information needed
+											//required - values = contest instance
+		},
+		templateUrl: 'views/contests/contestLabel.html',
+	};
+});
+
+/*
 * mrtContestList is the directive responsable to render a list of contest
 * Set to be used as element-only
-* has mrtContestListWrapper as required for proper struct of html, but they have no logical dependency
+* has mrtContestListWrapper as required for proper structure of html, but they have no logical dependency
 */
 app.directive('mrtContestList', function() {
 	return {
