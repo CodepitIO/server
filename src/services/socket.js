9af4a1b492b70706e2bf7e33c09f834cdf26e449
Gustavo Stor <gustavostor@gmail.com>
2016-06-09 10:09:05 -0300

    testing socket

--- src/services/socket.js
+++ src/services/socket.js
@@ -4,13 +4,21 @@ const socketIO = require('socket.io'),
 const REDIS = require('../config/constants').REDIS
 
 module.exports = function(server) {
+  var count = 0
   let io = socketIO(server)
   io.adapter(redisAdapter({ host: REDIS.HOST, port: REDIS.PORT }))
 
   io.on('connection', (socket) => {
+    count++
+    console.log(`connect, total users is ${count}`)
     socket.on('join', (data) => {
       data.previous && socket.leave(data.previous)
       data.current && socket.join(data.current)
+    })
+
+    socket.on('disconnect', () => {
+      count--
+      console.log(`disconnect, total users is ${count}`);
     })
   })
 }
