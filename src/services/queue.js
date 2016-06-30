d1e1de2ee97b7d01aaba09d4e6000709c663bd54
Gustavo Stor <gustavostor@gmail.com>
2016-06-30 19:05:47 -0300

    Removed unnecessary requires

--- src/services/queue.js
+++ src/services/queue.js
@@ -1,16 +1,6 @@
 'use strict'
 
-const async = require('async'),
-  fs = require('fs'),
-  kue = require('kue'),
-  path = require('path'),
-  util = require('util')
-
-const DownloadFile = require('../utils/utils').downloadFile,
-  Errors = require('../utils/errors'),
-  Problem = require('../models/problem')
-
-// --- SUBMISSION QUEUE ---
+const kue = require('kue')
 
 var SubmissionQueue = kue.createQueue({
   redis: {
