ab93b9c42283120906f0b8f0a10312eba2c67d6f
dgmneto <dgmn@cin.ufpe.br>
2016-12-14 22:40:06 -0300

    making mongoose retry connection on fail

--- src/services/dbs.js
+++ src/services/dbs.js
@@ -7,7 +7,15 @@ const MONGO = require('../config/constants').MONGO,
   REDIS = require('../config/constants').REDIS
 
 mongoose.Promise = require('bluebird')
-mongoose.connect(`mongodb://${MONGO.HOST}:${MONGO.PORT}/${MONGO.DB}`)
+var connectWithRetry = function() {
+  return mongoose.connect(`mongodb://${MONGO.HOST}:${MONGO.PORT}/${MONGO.DB}`, function(err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec');
+      setTimeout(connectWithRetry, 5000);
+    }
+  });
+};
+connectWithRetry();
 
 let redisClient = redis.createClient({
   host: REDIS.HOST,
